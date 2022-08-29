452996835ddd7271f9927cca5a8c9fab445679ff
graddaniel <48184737+graddaniel@users.noreply.github.com>
2022-08-29 11:21:25 +0200

    [KP-873] New Plugin JSON (#1)

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 const { bootstrap } = require("@kaholo/plugin-library");
+const { readFile, writeFile } = require("fs/promises");
+const { resolve } = require("path");
 
-async function hello(params) {
+async function readJsonFile(params) {
   const {
-    helloName,
-    saySecret,
-    secret,
+    filePath,
   } = params;
 
-  let greeting = `Hello ${helloName}!`;
+  const absoluteFilePath = resolve(filePath);
 
-  if (saySecret && !secret) {
-    throw new Error("No secret was provided to say. Please provide a secret or uncheck \"Say Secret\".");
-  }
+  const fileContent = await readFile(
+    absoluteFilePath,
+    {
+      encoding: "utf-8",
+      flag: "r",
+    },
+  );
 
-  if (saySecret) {
-    greeting += `\nHere is the secret: ${secret}`;
-  }
+  JSON.parse(fileContent);
 
-  return greeting;
+  return fileContent;
+}
+
+async function writeJsonFile(params) {
+  const {
+    filePath,
+    json, // parserType:object validates that for us
+  } = params;
+
+  const absoluteFilePath = resolve(filePath);
+  const jsonString = JSON.stringify(json);
+
+  await writeFile(
+    absoluteFilePath,
+    jsonString,
+    { encoding: "utf-8" },
+  );
 }
 
 module.exports = bootstrap({
-  hello,
+  readJsonFile,
+  writeJsonFile,
 });
